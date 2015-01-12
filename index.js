2a353dcc3593b94732eea939244a4a097730688b
Martin Svanberg <martin@martinsvanberg.com>
2015-01-11 19:53:57 -0800

    Add command line option parsing

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var to5 = require("6to5");
+var argv = require("minimist")(process.argv.slice(2));
 
 module.exports = {
   process: function (src, filename) {
+    // Use options passed in from the command line
+    var options = argv;
+    delete options._;
+    options.filename = filename;
+
     // Ignore all files within node_modules
     // 6to5 files can be .js and .es6
     if (filename.indexOf("node_modules") === -1 && to5.canCompile(filename)) {
-      return to5.transform(src, { filename: filename }).code;
+      return to5.transform(src, options).code;
     }
     return src;
   }
